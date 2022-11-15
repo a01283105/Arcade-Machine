2e2e56134dfde521a064a8dbf612fcb9239a9f1b
David Sanchez Ortiz <a01283105@itesm.mx>
2022-11-15 16:40:58 -0600

    Rotacion hecha

--- Arcade Machine/Assets/MoveCamera.cs
+++ Arcade Machine/Assets/MoveCamera.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    private Transform go;
-    float i = 0;
-    // Start is called before the first frame update
-    void Start()
+    float rotationspeed = 1f;
+
+    void OnMouseDrag()
     {
-        go = GetComponent<Transform>();
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.anyKeyDown)
-        {
-            i = i+1;
-        }
-        transform.rotation = new Quaternion(0.0f, i, 0.0f,0.0f);
+        float XaxisRotation = Input.GetAxis("Mouse X") * rotationspeed;
+        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationspeed;
+        transform.Rotate(Vector3.down, XaxisRotation);
+        transform.Rotate(Vector3.right, YaxisRotation);
     }
 }
